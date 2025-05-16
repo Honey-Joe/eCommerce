3affae607470a4eef49cfdc905146b6af8b20e90
Honey-Joe <honeyjoe942@gmail.com>
2025-05-16 12:09:37 +0530

    updtaed axios

--- admin/src/axios.js
+++ admin/src/axios.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://ecommerce-api-tawny-mu.vercel.app/api",
   withCredentials: true, // required to send HTTP-only cookies
 });
 
