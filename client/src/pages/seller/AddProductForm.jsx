158d1789e1c7b4b6036b345e7ae361efb2097408
Honey-Joe <honeyjoe942@gmail.com>
2025-05-26 23:27:29 +0530

    Disable brand and category selection in AddProductForm when a variant is selected

--- client/src/pages/seller/AddProductForm.jsx
+++ client/src/pages/seller/AddProductForm.jsx
@@ -260,7 +260,7 @@ const AddProductForm = () => {
           onChange={handleBrandChange}
           className="w-full p-2 border border-gray-300 rounded-lg text-sm"
           required
-          disabled={!!parentProduct}
+          disabled={isVariant}
         >
           <option value="">-- Select Brand --</option>
           {brands
@@ -295,7 +295,7 @@ const AddProductForm = () => {
           onChange={handleChange}
           className="w-full p-2 border border-gray-300 rounded-lg text-sm"
           required
-          disabled={!!parentProduct}
+          disabled={isVariant}
         >
           <option value="">-- Select Category --</option>
           {categories.map((cat) => (
