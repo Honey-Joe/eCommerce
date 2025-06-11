134219fffce2834b733394673eb652d8bc745b23
Honey-Joe <honeyjoe943@gmail.com>
2025-06-11 12:04:18 +0530

    fix: Add missing service configuration for nodemailer in sendInvoiceEmail utility

--- api/utils/sendInvoiceEmail.js
+++ api/utils/sendInvoiceEmail.js
@@ -6,6 +6,7 @@ const sendInvoiceEmail = async (order, pdfPath) => {
       host: process.env.SMTP_HOST,
       port: +process.env.SMTP_PORT,
       secure: false,
+      service:"gmail",
       auth: {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASS,
